4406bba95c28c414285ac5aad689d40071bcc494
tao <huynhthetao987@gmail.com>
2022-06-07 17:10:57 +0700

    xây dựng layout header

---
+++ src/components/Layout/components/AccountItem/index.js
@@ -0,0 +1,25 @@
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import styles from './AccountItem.module.scss';
+
+const cx = classNames.bind(styles);
+
+export default function AccountItem() {
+  return (
+    <div className={cx('wrapper')}>
+      <img
+        className={cx('avatar')}
+        src="https://p9-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/42e967d55a67f8f2e84b29ac67315232.jpeg?x-expires=1654765200&x-signature=KOl89ozZFXJklSR3aR0LKrffGHg%3D"
+        alt="bog"
+      />
+      <div className={cx('info')}>
+        <h4 className={cx('name')}>
+          <span>Nguyen Van A</span>
+          <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+        </h4>
+        <span className={cx('username')}>nguyenvana</span>
+      </div>
+    </div>
+  );
+}
