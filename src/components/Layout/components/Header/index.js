ac60f4dd7f24d843c575b946927434ab380639a7
tao <huynhthetao987@gmail.com>
2022-06-08 16:23:49 +0700

    add button more header

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,7 +1,15 @@
 import { useState, useEffect } from 'react';
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCircleXmark,
+  faSpinner,
+  faMagnifyingGlass,
+  faEllipsisVertical,
+  faEarthAsia,
+  faCircleQuestion,
+  faKeyboard,
+} from '@fortawesome/free-solid-svg-icons';
 import Tippy from '@tippyjs/react/headless';
 
 import styles from './Header.module.scss';
@@ -9,8 +17,15 @@ import images from '~/assets/images';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import AccountItem from '~/components/AccountItem';
 import Button from '~/components/Button';
+import Menu from '~/components/Popper/Menu';
 
 const cx = classNames.bind(styles);
+
+const MENU_ITEMS = [
+  { icon: <FontAwesomeIcon icon={faEarthAsia} />, title: 'English' },
+  { icon: <FontAwesomeIcon icon={faCircleQuestion} />, title: 'Feedback and help', to: '/feedback' },
+  { icon: <FontAwesomeIcon icon={faKeyboard} />, title: 'Keyboard shortcuts' },
+];
 
 export default function Header() {
   const [searchResult, setSearchResult] = useState([]);
@@ -54,6 +69,11 @@ export default function Header() {
         <div className={cx('action')}>
           <Button text>Upload</Button>
           <Button primary>Login</Button>
+          <Menu items={MENU_ITEMS}>
+            <button className={cx('more-btn')}>
+              <FontAwesomeIcon icon={faEllipsisVertical} />
+            </button>
+          </Menu>
         </div>
       </div>
     </header>
--- src/components/Popper/Wrapper.js
+++ src/components/Popper/Wrapper.js
@@ -3,6 +3,6 @@ import styles from './Popper.module.scss';
 
 const cx = classNames.bind(styles);
 
-export default function Wrapper({ children }) {
-  return <div className={cx('wrapper')}>{children}</div>;
+export default function Wrapper({ children, className }) {
+  return <div className={cx('wrapper', className)}>{children}</div>;
 }
