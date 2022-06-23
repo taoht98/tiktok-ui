06d87e688334e8972d570ba03f52383b830f2e95
tao <huynhthetao987@gmail.com>
2022-06-23 14:06:59 +0700

    change name files

---
+++ src/layouts/DefaultLayout/DefaultLayout.js
@@ -0,0 +1,18 @@
+import Header from '~/layouts/components/Header';
+import Sidebar from './Sidebar';
+import styles from './DefaultLayout.module.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+export default function DefaultLayout({ children }) {
+  return (
+    <div className={cx('wrapper')}>
+      <Header />
+      <div className={cx('container')}>
+        <Sidebar />
+        <div className={cx('content')}>{children}</div>
+      </div>
+    </div>
+  );
+}
